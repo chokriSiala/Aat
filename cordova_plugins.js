27216bcf96b131451e923f5c10a6f9b14a99eff4
https://github.com/chokriSiala/ <softtodo@mahmouds-imac.lan>
2014-06-26 17:11:55 +0100

    cordovaplugin

--- cordova_plugins.js
+++ cordova_plugins.js
@@ -76,12 +76,20 @@ module.exports = [
         "merges": [
             "Contact"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.contacts": "0.2.10"
+    "org.apache.cordova.contacts": "0.2.10",
+    "org.apache.cordova.dialogs": "0.2.8"
 }
 // BOTTOM OF METADATA
 });
